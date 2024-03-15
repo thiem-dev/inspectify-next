b9bee7eb2c241b5b9a2eed1c559d140306d50a47
Thiem Huynh <thiem.huynh0@gmail.com>
2024-03-14 22:50:48 -0400

    Fixing CORS issues

--- src/app/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request: Request) {
-  console.log('middleware');
-
-  console.log(request.method);
-  console.log(request.url);
-}
